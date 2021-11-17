8b05c0a54e283aca8b08efebcb15572af459389f
Trần Cường <trancuongitus@gmail.com>
2021-11-17 19:16:35 +0700

    Test commit
    Test Commit Description

--- src/main/java/cybersoft/java14/backend/crm/servlet/HomServlet.java
+++ src/main/java/cybersoft/java14/backend/crm/servlet/HomServlet.java
@@ -12,6 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 public class HomServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.getWriter().append("Test commit");
 		req.getRequestDispatcher("/WEB-INF/views/home.jsp").forward(req, resp);
 	}
 }
